c522dc0dd2cf14a9686107884208b019278e9183
Hannes Widrig <hanneswidrig@users.noreply.github.com>
2024-04-06 10:59:14 -0400

    chore: upgrade to zx v8

--- start.mjs
+++ start.mjs
@@ -5,8 +5,8 @@ import "zx/globals";
 await fs.rm("web-server", { recursive: true, force: true });
 await fs.mkdir("web-server");
 
-const startWebServer = () => $`cd web-server; pnpm dlx http-server --silent --cors`;
-const buildMicroUi = () => $`cd micro-ui; pnpm build`;
-const startShell = () => $`cd shell; pnpm dev`;
+const startWebServer = () => $({ quiet: true })`cd web-server; pnpm dlx http-server --silent --cors`;
+const buildMicroUi = () => $({ quiet: true })`cd micro-ui; pnpm build`;
+const startShell = () => $({ verbose: true })`cd shell; pnpm dev`;
 
 await Promise.all([startWebServer(), buildMicroUi(), startShell()]);
